a3027201c82697c05c48c377043b870a2173bca5
tangalbert919 <tangalbert919@gmail.com>
2018-05-08 19:41:16 -0500

    weeb.py: lewd and a lot of pokin'
    Very self-explanatory there. Just don't poke excessively.

--- cogs/weeb.py
+++ cogs/weeb.py
@@ -70,6 +70,26 @@ class Weeb:
         embed.set_image(url=url)
         embed.set_footer(text="Powered by weeb.sh")
         await ctx.send(embed=embed)
+    
+    @commands.command()
+    async def lewd(self, ctx):
+        """What a lewd!"""
+        if ctx.message.channel.is_nsfw():
+            async with self.bot.session.get('https://api-v2.weeb.sh/images/random?type=lewd&nsfw=true', headers={'Authorization': self.bot.settings.weebtoken, 'User-Angent': 'Monika/1.0.0'}) as url:
+                url = await url.json()
+                url = url.get("url")
+        else:
+            async with self.bot.session.get('https://api-v2.weeb.sh/images/random?type=lewd', headers={'Authorization': self.bot.settings.weebtoken, 'User-Angent': 'Monika/1.0.0'}) as url:
+                url = await url.json()
+                url = url.get("url")
+        if ctx.message.guild is not None:
+            color = ctx.message.guild.me.color
+        else:
+            color = discord.Colour.blue()
+        embed = discord.Embed(color=color, title="Lewd!", description="You are such a lewd, {}".format(ctx.message.author.name))
+        embed.set_image(url=url)
+        embed.set_footer(text="Powered by weeb.sh")
+        await ctx.send(embed=embed)
 
     @commands.command()
     @checks.is_upvoter()
@@ -112,6 +132,26 @@ class Weeb:
         else:
             color = discord.Colour.blue()
         embed = discord.Embed(color=color, title="Insult!", description="Oh! {} insulted {}!".format(ctx.message.author.name, user.name))
+        embed.set_image(url=url)
+        embed.set_footer(text="Powered by weeb.sh")
+        await ctx.send(embed=embed)
+
+    @commands.command()
+    async def poke(self, ctx, user: discord.Member):
+        """Oh! Pokes the specified user."""
+        if ctx.message.channel.is_nsfw():
+            async with self.bot.session.get('https://api-v2.weeb.sh/images/random?type=poke&nsfw=true', headers={'Authorization': self.bot.settings.weebtoken, 'User-Agent': 'Monika/1.0.0'}) as url:
+                url = await url.json()
+                url = url.get("url")
+        else:
+            async with self.bot.session.get('https://api-v2.weeb.sh/images/random?type=poke', headers={'Authorization': self.bot.settings.weebtoken, 'User-Agent': 'Monika/1.0.0'}) as url:
+                url = await url.json()
+                url = url.get("url")
+        if ctx.message.guild is not None:
+            color = ctx.message.guild.me.color
+        else:
+            color = discord.Colour.blue()
+        embed = discord.Embed(color=color, title="Poke!", description="{} poked {}...".format(ctx.message.author.name, user.name))
         embed.set_image(url=url)
         embed.set_footer(text="Powered by weeb.sh")
         await ctx.send(embed=embed)

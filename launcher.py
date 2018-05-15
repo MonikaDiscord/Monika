9541cdc57f8d61c5069477532dd245a108a846ed
kanikanikun! <32210799+Kanielyochanan@users.noreply.github.com>
2018-05-14 20:17:51 -0500

    Create launcher.py

---
+++ launcher.py
@@ -0,0 +1,13 @@
+from monika import Monika
+from argparse import ArgumentParser
+
+parser = argparse.ArgumentParser(description='Monika - the Discord bot!')
+parser.add_argument('--testing', help='doesn't load any cogs (and other small things)', action='store_true')
+args = parser.parse_args()
+if args.testing:
+  bot = Monika(testing=True)
+else:
+  bot = Monika()
+
+if __name__ == "__main__":
+  bot.run()

1553dc615ab8c34b86246c1f40ef00de3c495882
kaniel's mom <reidtj@zoho.com>
2018-05-22 20:25:39 -0500

    totally fucking tubular!

--- monika.py
+++ monika.py
@@ -15,7 +15,7 @@ class Monika(commands.AutoShardedBot):
         dbpass = self.config['dbpass']
         dbuser = self.config['dbuser']
         govinfo = {"user": dbuser, "password": dbpass, "database": "monika", "host": "localhost"}
-        
+
         async def _init_db():
             self.db = await asyncpg.create_pool(**govinfo)
             await self.db.execute("CREATE TABLE IF NOT EXISTS users (id bigint primary key, name text, discrim varchar (4), money text, patron int, staff int, upvoter boolean);")
@@ -25,14 +25,14 @@ class Monika(commands.AutoShardedBot):
 
         self.rclient = Client(self.config['sentry_dsn'])
 
-        def _prefixcall(bot, msg):
+        async def _prefixcall(bot, msg):
             if msg.guild is None: return commands.when_mentioned_or('$!')
             sql = "SELECT prefix FROM guilds WHERE id = $1"
             return await self.db.fetchval(sql, msg.guild.id)
 
         def _runprefixcall(bot, msg):
             self.loop.run_until_complete(_prefixcall(bot, msg))
-        
+
         super().__init__(command_prefix=_runprefixcall,
                          description="Hi, I'm Monika! Welcome to the Literature Club! Here are my commands:",
                          pm_help=None)
@@ -45,7 +45,7 @@ class Monika(commands.AutoShardedBot):
                 except:
                     print(f"Oops! I broke the {file} module...")
                     self.rclient.captureException()
-                    
+
     def get_prefix(msg):
         return _runprefixcall(self, msg)
 

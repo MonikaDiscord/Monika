dbe859fe8364bc778b78953b318f5594aeb859b5
kaniel <32210799+kanielyaniel@users.noreply.github.com>
2018-06-20 21:55:44 -0400

    Update checks.py

--- utilities/checks.py
+++ utilities/checks.py
@@ -6,7 +6,7 @@ import json
 class Checks:
 
     def __init__(self):
-        asyncio.get_event_loop().run_until_complete(self._init_db())
+        asyncio.ensure_future(self._init_db())
 
     async def _init_db(self):
         config = json.loads(open('config.json', 'r').read())
